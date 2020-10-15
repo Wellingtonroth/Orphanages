b98a4877a6ada555a08aa5ab52f44af89d60fb01
Wellington Roth <wellington_roth@hotmail.com>
2020-10-15 13:04:21 -0300

    upload images with multer

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import multer from 'multer';
+import uploadConfig from './config/upload';
 import OrphanagesController from "./controllers/OrphanagesController";
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 //PARAMETROS
 
@@ -18,6 +21,6 @@ routes.get('/orphanages', OrphanagesController.index);
 
 routes.get('/orphanages/:id', OrphanagesController.show);
 
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 export default routes;
